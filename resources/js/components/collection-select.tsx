2de1fd806cc55b897cc8bfd1ba9dd8335b801d15
James Makela <jamesilmarimakela@outlook.com>
2025-03-04 20:45:25 +0800

    Change selector to be more generic.

---
+++ resources/js/components/collection-select.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import {
+    Select,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+    SelectContent,
+} from '@/components/ui/select';
+import axios from 'axios';
+
+interface Collection {
+    id: number;
+    name: string;
+    color: string;
+    icon: string;
+}
+
+interface CollectionSelectProps {
+    value: number;
+    collectionLocation: string;
+    placeholder: string;
+    onChange: (value: number) => void;
+}
+
+const CollectionSelect = ({ value, collectionLocation, placeholder, onChange }: CollectionSelectProps) => {
+    const [items, setItems] = useState<Collection[]>([]);
+
+    useEffect(() => {
+        // fetch the data from the laravel backend
+        axios.get(collectionLocation)
+            .then((response) => {
+                setItems(response.data);
+            })
+            .catch((error) => {
+                  console.error('Error fetching data:', error);
+            });
+
+    }, );
+
+    return (
+        <Select value={String(value)} onValueChange={(value) => onChange(Number(value))}>
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder={placeholder} />
+            </SelectTrigger>
+            <SelectContent>
+            {items.map((item) => (
+                    <SelectItem key={item.id} value={String(item.id)}>
+                        {item.name}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    );
+};
+
+export default CollectionSelect
+
