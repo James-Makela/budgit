349687f8ac081ad7397400b7916d7c520c77bda4
James Makela <jamesilmarimakela@outlook.com>
2025-03-30 07:42:07 +0800

    Add Transaction model.

---
+++ database/migrations/2025_03_29_070705_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('date');
+            $table->string('description');
+            $table->integer('credit_cents')->nullable();
+            $table->integer('debit_cents')->nullable();
+            $table->integer('balance_cents');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
